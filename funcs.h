7eb7d823a2d5a335078aaf7e87c0519aaef8083b
artemgon <artemhoncharuk123@gmail.com>
2024-11-09 14:11:31 +0200

    PE task 1 is done.

--- funcs.h
+++ funcs.h
@@ -1,1 +1,12 @@
 #include "libs.h" 
+
+template<class T>
+void myFunc(T* ptr, int size, T* s, T* p)
+{
+	for (int i = 0; i < size; i++)
+	{
+		*s += *(ptr + i);
+		*p *= *(ptr + i);
+	}
+	return;
+}
--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
+	const int size = 5;
+	int array[size] = { 5, 7, 2, 4, 6 }, *ptr = array,
+	sum = 0, product = 1;
+	myFunc(array, size, &sum, &product);
+	cout << "Sum: " << sum << endl;
+	cout << "Product: " << product << endl;
 	return 0;
 }
