a3b9d21fa4543df167d9cbaf1cabcf04658b66b1
artemgon <artemhoncharuk123@gmail.com>
2024-11-10 18:46:22 +0200

    Task 4 is done.

--- funcs.h
+++ funcs.h
@@ -1,25 +1,77 @@
 #include "libs.h" 
 
 template<class T>
-void myFunc(T* ptr_1, int size_1, T* ptr_2, int size_2, int *index)
+bool checkFunc(T* ptr, int size)
 {
-	int counter = 0;
-	for (int i = 0; i < size_2; i++)
-	{
-		for (int j = 0; j < size_1; j++)
-		{
-			if (*(ptr_2 + i) == *(ptr_1 + j))
-			{
-				if (i == 0)
-				{
-					*index = j;
-				}
-				counter++;
-			}
-		}
-	}
-	if (counter != size_2)
-	{
-		*index = 0;
-	}
+    bool c_var = false;
+    for (int i = 0; i < size; i++)
+    {
+        if (*(ptr + i) < 0)
+        {
+            c_var = true;
+            break;
+        }
+    }
+    return c_var;
+}
+
+template<class T>
+void deleteFunc(T* ptr, int &size, int index)
+{
+    for (int j = index; j < size - 1; j++)
+    {
+
+        *(ptr + j) = *(ptr + j + 1);
+    }
+    size--;
+}
+
+template<class T>
+void myFunc(T* ptr, int &size)
+{
+    bool check_var = checkFunc(ptr, size);
+    int i = 0;
+    if (check_var)
+    {
+        while (i < size)
+        {
+            if (*(ptr + i) < 0 && i < size - 1)
+            {
+                while (*(ptr + i) < 0)
+                {
+                    deleteFunc(ptr, size, i);
+                }
+                i++;
+            }
+            else if (*(ptr + i) < 0 && i == size - 1)
+            {
+                --size;
+                break;
+            }  
+            else
+                i++;
+        }
+    }
+}
+
+template<class T>
+void arrayFill(T* ptr, int &size)
+{
+    cout << "Enter an array: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> *(ptr + i);
+        cout << " ";
+    }
+}
+
+template<class T>
+void printArr(T* ptr, int &size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << *ptr << " ";
+        ++ptr;
+    }
+    cout << endl;
 }
--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 
 int main()
 {
-	const int size_1 = 6, size_2 = 3;
-	int A[size_1] = { 5, -7, 2, -4, 0 , 0 }, * ptr_1 = A,
-		B[size_2] = { -7, 2, -4 }, * ptr_2 = B, index;
-	myFunc(A, size_1, B, size_2, &index);
-	cout << "Index: " << index << endl;
+	int size = 5;
+	int* array = new int [size];
+	arrayFill(array, size);
+	myFunc(array, size);
+	printArr(array, size);
+	delete[] array;
 	return 0;
 }
