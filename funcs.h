92add00acc628f196ef766b38d38a550d9f607ce
artemgon <artemhoncharuk123@gmail.com>
2024-11-11 09:37:28 +0200

    Task 6 is done.

--- funcs.h
+++ funcs.h
@@ -11,13 +11,31 @@ void fillArr(T* ptr, int size)
 	return;
 }
 
-template<class T>
-void myFunc(T* ptr_1, int &size_1, T* ptr_2, int size_2)
+template<typename T>
+void addPlace(T* ptr, int &size, int index)
 {
-	int size_1_1 = size_1;
-	size_1 = size_1 + size_2;
-	for (int i = size_1_1, j = 0; i < size_1 && j < size_2; i++, j++)
+	size++;
+	for (int i = size - 1; i > index; i--)
 	{
+		if (i - 1 == index)
+		{
+			*(ptr + i) = *(ptr + i - 1);
+			break;
+		}
+		*(ptr + i) = *(ptr + i - 1);
+	}
+	return;
+}
+
+template<class T>
+void myFunc(T* ptr_1, int &size_1, T* ptr_2, int size_2, int index)
+{
+	for (int i = 0; i < size_2; i++)
+	{
+		addPlace(ptr_1, size_1, index);
+	}
+	for (int i = index, j = 0; i < size_2 + index && j < size_2; i++, j++)
+	{	
 		*(ptr_1 + i) = *(ptr_2 + j);
 	}
 	return;
--- main.cpp
+++ main.cpp
@@ -2,14 +2,17 @@
 
 int main()
 {
-	int size_1 = 6, size_2 = 3;
+	int size_1 = 6, size_2 = 3, index;
 	int* array = new int[size_1], * block = new int[size_2];
 	cout << "Enter array: " << endl;
 	fillArr(array, size_1);
 	cout << endl << "Enter block: " << endl;
 	fillArr(block, size_2);
 	cout << endl;
-	myFunc(array, size_1, block, size_2);
+	cout << endl << "Enter index: " << endl;
+	cin >> index;
+	cout << endl;
+	myFunc(array, size_1, block, size_2, index);
 	printFunc(array, size_1);
 	return 0;
 }
