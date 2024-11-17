2a56dc2b0a63eaec22a4d7bffa1a774bb86350fc
artemgon <artemhoncharuk123@gmail.com>
2024-11-17 20:08:55 +0200

    Task 2 is done.

--- funcs.h
+++ funcs.h
@@ -1,1 +1,60 @@
 #include "libs.h" 
+
+template<typename T>
+void initFunc(T* ptr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cin >> *ptr;
+		++ptr;
+	}
+	return;
+}
+
+template<typename T>
+void printFunc(T* ptr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << *ptr << " ";
+		++ptr;
+	}
+	return;
+}
+
+template<typename T>
+bool checkFunc(T elem)
+{
+    if (elem < 2) return false; 
+    for (T i = 2; i * i <= elem; ++i) {
+        if (elem % i == 0) return false;
+    }
+    return true;
+}
+
+template<typename T>
+T* deleteElem(T *ptr, int& size, int index) {
+    T* ptr_1 = new T[size - 1];
+    for (int i = 0, j = 0; i < size; ++i) {
+        if (i != index) { 
+            ptr_1[j++] = ptr[i];
+        }
+    }
+    --size;
+    delete[] ptr;
+    return ptr_1;
+}
+
+
+template<typename T>
+void myFunc(T* &ptr, int& size)
+{
+    for (int i = 0; i < size; ) { 
+        if (checkFunc(*(ptr + i))) { 
+            ptr = deleteElem(ptr, size, i); 
+        }
+        else {
+            ++i;
+        }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 
 int main()
 {
+	int size = 6;
+	int* array = new int[size];
+	cout << "Enter an array: " << endl;
+	initFunc(array, size);
+	cout << endl;
+	printFunc(array, size);
+	cout << endl;
+	myFunc(array, size);
+	cout << endl;
+	printFunc(array, size);
+	delete[] array;
 	return 0;
 }
